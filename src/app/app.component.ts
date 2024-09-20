fc7c01b2368f0686abe404b42f05d58ad3a3f243
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-09-20 10:16:57 +0200

    localStorage db added, save to / load from LocalStorage functions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Project } from './models/project';
 
 @Component({
@@ -6,27 +6,40 @@ import { Project } from './models/project';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.loadData();
+  }
   title = 'probafeladat';
 
   projects: Project[] = [];
   project: Project = new Project();
   editEnabled: boolean = false;
+  projectLocalDb: string = 'projectLocalDb';
+
+  // Saves Data to LocalStorage
+  saveData(projects: Project[]) {
+    this.projects = projects;
+    localStorage.setItem(this.projectLocalDb, JSON.stringify(this.projects));
+  }
 
   /// CREATE
   addProject() {
     this.projects.unshift(Object.assign(new Project(), this.project));
+    this.saveData(this.projects);
   }
   /// READs data from LocalStorage
-  loadData() {}
+  loadData() {
+    let data = JSON.parse(localStorage.getItem(this.projectLocalDb) ?? '[]');
+    this.projects = Object.values(data).map((x) =>
+      Object.assign(new Project(), x)
+    );
+  }
   /// UPDATE
   saveEdits() {}
   cancelEdits() {}
   /// DELETE - by clickin on the trash btn
   deleteProject(project: Project) {}
-
-  // Saves Data to LocalStorage
-  saveData() {}
 
   // Loads project data to the inputfields
   loadEdit(project: Project) {}
